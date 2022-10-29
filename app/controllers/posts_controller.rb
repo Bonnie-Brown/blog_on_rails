aba018b9d21e23bc92bd077f0174e770c71f87a7
Bonnie Brown <bonnie.brown@jane.app>
2022-10-29 22:59:40 +0000

    Aded post controllerr actions, routes and view pages.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,13 @@
 class PostsController < ApplicationController
 
+    # Callbacks
+
+    before_action :find_post, only: [:edit, :update, :show, :destroy]
+
+    # Actions
+
     def index
+        @posts = Post.order(created_at: :desc)
     end
 
     def show
@@ -12,7 +19,6 @@ class PostsController < ApplicationController
 
     def create
         @post = Post.new(post_params)
-
         if @post.save
             flash[:success] = "Post successfully created."
             redirect_to @post
@@ -20,20 +26,33 @@ class PostsController < ApplicationController
             flash[:error] = "Something went wrong"
             render 'new'
         end
-            
-
     end
 
     def edit
     end
 
     def update
+        if @post.update(post_params)
+            flash[:success] = "Post successfully created."
+            redirect_to @post
+        else
+            flash[:error] = "Something went wrong"
+            render 'edit' 
+        end
     end
 
     def destroy
+        # @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to root_path
     end
 
     private
+
+    def find_post
+        @post = Post.find(params[:id])
+
+    end
 
     def post_params
         params.require(:post).permit(:title, :body)
