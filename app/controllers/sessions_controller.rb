53014f6205e26b6bdb5ca3493fba42c08f2c0a9f
Bonnie Brown <bonnie.brown@jane.app>
2022-11-05 22:09:42 +0000

    Added Sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+
+    def new
+    end
+
+
+    def create
+        @user = User.find_by_email[:email]
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to root_path, {notice: "Logged in"}
+        else
+            render :new, {alert: "Wrong email or password", status: 303} 
+        end
+    end
+
+
+    def destroy
+        session[:user_id] = nil
+        {notice: "Logged out"}
+        redirect_to root_path
+    end
+
+
 end
