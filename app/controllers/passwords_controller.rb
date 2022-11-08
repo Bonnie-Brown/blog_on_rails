fe0893f00e7c3a13f7def49acb9f79cc911aaece
Bonnie Brown <bonnie.brown@jane.app>
2022-11-08 06:06:33 +0000

    User needs to input correct current password in order to update now.

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -4,6 +4,7 @@ class PasswordsController < ApplicationController
   before_action :user_signed_in?
   before_action :find_user, only: [:edit, :update]
   before_action :authenticate_user!
+#   before_action :compare_passwords, only: [:update]
 
   
 # Action
@@ -15,10 +16,24 @@ class PasswordsController < ApplicationController
 
     def update
 
-        if  @user.update(password_params)
-            redirect_to root_path, notice: "Password Updated."
+        if @user.authenticate(params[:user][:current_password])
+
+            # if compare_passwords?
+
+            #     render 'edit'
+
+            # else
+
+                if  @user.update(password_params)
+                    redirect_to root_path, notice: "Password Updated."
+                else
+                    render 'edit'
+                end
+            
+            # end
         else
             render 'edit'
+
         end
     end
 
@@ -26,11 +41,24 @@ class PasswordsController < ApplicationController
 
     def find_user
     
-    @user = @current_user
+     @user = @current_user
 
     end
 
     def password_params
         params.require(:user).permit(:password, :password_confirmation)
     end
+
+    # def compare_passwords?
+    #     @current_password = :current_password
+    #     @password = :password
+
+    #     if @current_password == @password
+    #         return true
+    #     else
+    #         return false
+    #     end
+    
+    # end
+
 end
